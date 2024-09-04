d9a3944d458ca60e8c93356d3ec891ccf95cabb5
StaCKm29 <mamartinez2023@udec.cl>
2024-09-04 16:18:32 -0400

    Solo archivos makefile modificados

---
+++ cmake-build-debug/CMakeFiles/shell.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shell.dir/src/alarma.c.o"
+  "CMakeFiles/shell.dir/src/alarma.c.o.d"
+  "CMakeFiles/shell.dir/src/ejecutarComandos.c.o"
+  "CMakeFiles/shell.dir/src/ejecutarComandos.c.o.d"
+  "CMakeFiles/shell.dir/src/main.c.o"
+  "CMakeFiles/shell.dir/src/main.c.o.d"
+  "CMakeFiles/shell.dir/src/tokenEspacios.c.o"
+  "CMakeFiles/shell.dir/src/tokenEspacios.c.o.d"
+  "CMakeFiles/shell.dir/src/tokenPipes.c.o"
+  "CMakeFiles/shell.dir/src/tokenPipes.c.o.d"
+  "shell"
+  "shell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/shell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
